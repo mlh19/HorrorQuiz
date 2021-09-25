454e3a8557ba2723467160959d42d8ed76681cc8
Mary Lou <ml.hoover@yahoo.com>
2021-09-24 20:40:16 -0500

    Created files/folders necessary for application. Started creating HTML & JS.

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var questions = [{
+    question : "In which year did the original movie, Physco by Alfred Hitchcock debuet?"
+    answer : "1960"
+    choices : ["1960", "1964", "1984", "1980"]
+},
+{   question : 
+    answer : 
+    choices : 
+},
+{   question : 
+    answer : 
+    choices : 
+},
+{   question : 
+    answer : 
+    choices : 
+},
+{
+    question : 
+    answer :
+    choices :  
+}
+]
