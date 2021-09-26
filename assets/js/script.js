a1178a18f6f279bce9965aa4c9ff70a23c237ed1
Mary Lou <ml.hoover@yahoo.com>
2021-09-26 16:41:47 -0500

    Fix syntax issues in JS and begin to work on HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,29 @@
-var questions = [{
-    question : "In which year did the original movie, Physco by Alfred Hitchcock debuet?"
-    answer : "1960"
-    choices : ["1960", "1964", "1984", "1980"]
-},
-
-{   
-    question : "Which of these movies features Ed & Lorraine Warren"
-    answer : "The Conjuring"
-    choices : ["Drag me to Hell", "Insidous", "The Conjuring", "Paranormal Activity"]
-}
-
-{   
-    question : "What movie is the following quote from? 'This is no dream! This is really happening'"
-    answer : "Rosemary's Baby"
-    choices : ["Scream", "Nightmare on Elm Street", "Silence of the Lambs", "Rosemary's Baby"]
-}
-
-{   
-    question : "How many days did it take to create the Blair Witch Project?"
-    answer : "8 days"
-    choices : ["7 days", "8 days", "20 days", "22 days"]
-}
-
-{
-    question : "Why did the parents kill the human Freddy Krueger?"
-    answer : "He killed children"
-    choices :  ["The parents weren't fond of his sweater", "Freddy was possesed", "Freddy killed children", "The parents didn't like Freddy living on their street. "]
-}
-
-]
+// array of questions, choices, and answers
+var questions = [
+    {
+        question : "In which year did the original movie, Physco by Alfred Hitchcock debuet?",
+        choices : ["1960", "1964", "1984", "1980"],
+        answer : "1960"
+    },
+    {
+        question : "Which of these movies features Ed & Lorraine Warren",
+        choices : ["Drag me to Hell", "Insidous", "The Conjuring", "Paranormal Activity"],
+        answer : "The Conjuring"   
+    },
+    {
+        question : "What movie is the following quote from? 'This is no dream! This is really happening'",
+        choices : ["Scream", "Nightmare on Elm Street", "Silence of the Lambs", "Rosemary's Baby"],
+        answer : "Rosemary's Baby"
+    }, 
+    {
+        question : "How many days did it take to create the Blair Witch Project?",
+        choices : ["7 days", "8 days", "20 days", "22 days"],
+        answer : "8 days"
+    },
+    {
+        question : "Why did the parents kill the human Freddy Krueger?",
+        choices :  ["The parents weren't fond of his sweater", "Freddy was possesed", "Freddy killed children", "The parents didn't like Freddy living on their street. "],
+        answer : "He killed children"
+        
+    }
+];
