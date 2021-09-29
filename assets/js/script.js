17eca4b11ecf424a8cddb90ae7610c7e9dac8ae2
Mary Lou <ml.hoover@yahoo.com>
2021-09-28 21:37:18 -0500

    Finish the implementation of choosing right and wrong choices.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Elements
-var countdownEl = document.getElementById ("countdown")
+var countdownEl = document.getElementById("countdown")
 var startButton = document.getElementById("startButton")
 var title = document.getElementById ("title") 
 var quizEl = document.getElementById ("quiz")
@@ -7,8 +7,8 @@ var quizEl = document.getElementById ("quiz")
 // Quiz Variables
 var currentQuestionIndex = 0
 var countdown = 60 
-// You need a way to track the user's corect questions.
-
+// It will increment by 20 for each question right.
+var score = 0
 
 // array of questions, choices, and answers
 var questions = [
@@ -40,7 +40,7 @@ var questions = [
 ];
 
 function timer() {
-    countdownEl.textContent = "60";
+    countdownEl.textContent = countdown;
 
     setInterval(function() { 
         console.log("yes")
@@ -60,30 +60,45 @@ function startQuiz() {
 
 //Quiz Questions
 function getQuestion() {
-    // Get the current question based on the index which determines
-    // what question the user is on.
+    // Get the current question based on the index which determines what question the user is on.
    var question = questions[currentQuestionIndex];
-//    console.log(question1) 
+   console.log("The answer is " + question.answer);
    title.textContent = question.question;
+
+   // Loops the current question's choices to create a button with their titles as
+   // each question choice.
    for (let i = 0; i < question.choices.length; i++) {
-       const element = question.choices[i];
-    //    console.log(element)
-       var choicesBtn = document.createElement("button");
-       choicesBtn.textContent= element;
-       quizEl.append(choicesBtn);
-       // When a choices btn is clicked, it will call the choiceButtonClicked function.
-       choicesBtn.addEventListener("click", choiceButtonClicked)
+       const questionChoice = question.choices[i];
+    // Give each choice button names button1, button2, etc. so we can know 
+    // later which one the user selected.
+       var choiceButton = document.createElement("button");
+       choiceButton.textContent = questionChoice;
+       // Add each choice button to the quizEl div.
+       quizEl.append(choiceButton);
+       // When a choices button is clicked, it will call the choiceButtonClicked function.
+       choiceButton.addEventListener("click", choiceButtonClicked)
    }
 }
 
 // Each answer choice button calls this function.
 function choiceButtonClicked() {
-    
-    var buttonAll = document.querySelectorAll ("button");
-    
-    for (let i = 0; i < buttonAll.length; i++) {
-    buttonAll[i].remove();
+    // this refers to the current object that called this function.
 
+    // Check if the text of the button clicked matches the current question's answer.
+    if (this.textContent == questions[currentQuestionIndex].answer) {
+        // score = score + 20;
+        score += 20;
+        console.log("You got the question right. Your current score is " + score);
+    } else {
+        console.log("You got the question wrong.");
+        countdown -= 5;
+        // Update the UI immediately instead of waiting for the next timer cycle.
+        countdownEl.textContent = countdown;
+    }
+
+    var buttonAll = document.querySelectorAll("button");
+    for (let i = 0; i < buttonAll.length; i++) {
+        buttonAll[i].remove();
     }
 
     // Increment the question index to get the next question when it will reload the quiz.
